d36666b0e3c14fbc2f84af2c97be8caf59412944
Saidane Marwan <saidane.marwan@hotmail.fr>
2021-12-23 18:28:59 +0000

    Update du fichier cart.js : Ajout de la fonction displayTotals, qui permet d'afficher le total quantité et le total prix

--- front/js/cart.js
+++ front/js/cart.js
@@ -101,26 +101,32 @@ function displayCart() {
 
 displayCart();
 
-/* <article class="cart__item" data-id="{product-ID}" data-color="{product-color}">
-                <div class="cart__item__img">
-                  <img src="../images/product01.jpg" alt="Photographie d'un canapé">
-                </div>
-                <div class="cart__item__content">
-                  <div class="cart__item__content__description">
-                    <h2>Nom du produit</h2>
-                    <p>Vert</p>
-                    <p>42,00 €</p>
-                  </div>
-                  <div class="cart__item__content__settings">
-                    <div class="cart__item__content__settings__quantity">
-                      <p>Qté : </p>
-                      <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="42">
-                    </div>
-                    <div class="cart__item__content__settings__delete">
-                    
-                      <p class="deleteItem">Supprimer</p>
-                    </div>
-                  </div>
-                </div>
-</article> */
-              
+function displayTotals() {
+
+    // Affichage de la quantité total
+    let inputQuantity = document.getElementsByClassName("itemQuantity");
+    let totalLength = inputQuantity.length;
+    let totalQuantity = 0
+
+    for (let i = 0; i < totalLength; i++) {
+        totalQuantity += inputQuantity[i].valueAsNumber;
+    }
+
+    let displayQuantity = document.getElementById("totalQuantity");
+    displayQuantity.innerHTML = totalQuantity;
+    console.log(totalQuantity);
+
+    // Affichage du prix total
+    let totalPrice = 0;
+
+    for (let i = 0; i < totalLength; i++) {
+        totalPrice += (inputQuantity[i].valueAsNumber * productCart[i].kanapPrice);
+    }
+
+    let displayPrice = document.getElementById("totalPrice");
+    displayPrice.innerHTML = totalPrice;
+    console.log(totalPrice);
+}
+
+displayTotals();
+
